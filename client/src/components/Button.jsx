fdf21950963448c35b9c9bb4020ccc336e5d87f8
TapamitaMondal07 <tapomita.tae007@gmail.com>
2025-02-04 03:44:06 +0530

    fix the button and counts card components

--- client/src/components/Button.jsx
+++ client/src/components/Button.jsx
@@ -2,7 +2,7 @@ import { CircularProgress } from "@mui/material";
 import React from "react";
 import styled from "styled-components";
 
-const Button = styled.div`
+const StyledButton = styled.button`
   border-radius: 10px;
   color: white;
   font-size: 14px;
@@ -16,92 +16,49 @@ const Button = styled.div`
   padding: 16px 26px;
   box-shadow: 1px 20px 35px 0px ${({ theme }) => theme.primary + 40};
   border: 1px solid ${({ theme }) => theme.primary};
+  background: ${({ type, theme }) => (type === "secondary" ? theme.secondary : theme.primary)};
+  
   @media (max-width: 600px) {
     padding: 8px 12px;
   }
 
-  ${({ type, theme }) =>
-    type === "secondary"
-      ? `
-  background: ${theme.secondary};
-border: 1px solid ${({ theme }) => theme.secondary};
-  `
-      : `
-  background: ${theme.primary};
-`}
-
-  ${({ isDisabled }) =>
-    isDisabled &&
+  ${({ disabled }) =>
+    disabled &&
     `
-  opacity: 0.8;
-  cursor: not-allowed;
-
+      opacity: 0.6;
+      cursor: not-allowed;
   `}
-  ${({ isLoading }) =>
-    isLoading &&
-    `
-    opacity: 0.8;
-  cursor: not-allowed;
-`}
-${({ flex }) =>
-    flex &&
-    `
-    flex: 1;
-`}
-
-${({ small }) =>
-    small &&
-    `
-padding: 10px 28px;
-`}
+  
+  ${({ flex }) => flex && `flex: 1;`}
+  ${({ small }) => small && `padding: 10px 28px;`}
   ${({ outlined, theme }) =>
     outlined &&
     `
-background: transparent;
-color: ${theme.primary};
-  box-shadow: none;
-`}
-  ${({ full }) =>
-    full &&
-    `
-  width: 100%;`}
+      background: transparent;
+      color: ${theme.primary};
+      box-shadow: none;
+  `}
+  ${({ full }) => full && `width: 100%;`}
 `;
 
-const button = ({
-  text,
-  isLoading,
-  isDisabled,
-  rightIcon,
-  leftIcon,
-  type,
-  onClick,
-  flex,
-  small,
-  outlined,
-  full,
-}) => {
+const Button = ({ text, isLoading, disabled, rightIcon, leftIcon, type, onClick, flex, small, outlined, full }) => {
   return (
-    <Button
-      onClick={() => !isDisabled && !isLoading && onClick()}
-      isDisabled={isDisabled}
+    <StyledButton
+      onClick={!disabled && !isLoading ? onClick : undefined}
+      disabled={disabled || isLoading}
       type={type}
-      isLoading={isLoading}
       flex={flex}
       small={small}
       outlined={outlined}
       full={full}
     >
-      {isLoading && (
-        <CircularProgress
-          style={{ width: "18px", height: "18px", color: "inherit" }}
-        />
-      )}
+      {isLoading && <CircularProgress style={{ width: "18px", height: "18px", color: "inherit" }} />}
       {leftIcon}
       {text}
       {isLoading && <> . . .</>}
       {rightIcon}
-    </Button>
+    </StyledButton>
   );
 };
 
-export default button;
+export default Button;
